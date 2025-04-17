881c51906f8e500412f9eed4e6d6e6e5ecf1ba84
Brian2994 <brianpari29@gmail.com>
2025-04-16 23:52:58 -0300

    Modifiquei o formulario e adicionei o css e js

---
+++ HTML/assets/js/script_formulario.js
@@ -0,0 +1,10 @@
+function mostrarLogin() {
+    document.getElementById("cadastroForm").classList.remove("active");
+    document.getElementById("loginForm").classList.add("active");
+  }
+  
+  function mostrarCadastro() {
+    document.getElementById("loginForm").classList.remove("active");
+    document.getElementById("cadastroForm").classList.add("active");
+  }
+  
