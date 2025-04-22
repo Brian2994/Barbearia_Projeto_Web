c97a969758ac7faee8669b85a8aaf9589be93a17
Samuel Santos <samueldsantos@ipt.br>
2025-04-22 16:17:57 -0300

    melhorias no código index e mudanças feitas nos arquivos

--- PHP/formulario.php
+++ PHP/formulario.php
@@ -6,9 +6,10 @@ $pass = 'testesenha123';
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $pass);
-    echo "<h2 style='color:green;'>Conexão com o banco de dados bem sucedida. </h2";
+    echo "<h2 style='color:green;'>Conexão com o banco de dados bem sucedida.</h2>";
 } 
 catch (PDOException $e) {
     echo "<h2 style='color:red;'>Erro na conexão: " . $e->getMessage() . "</h2>";
 }
 ?>
+
