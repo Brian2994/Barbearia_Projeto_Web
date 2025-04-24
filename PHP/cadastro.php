dab9e45d70d97965bf5492fcc4dadd27d08954ac
Samuel Santos <samueldsantos@ipt.br>
2025-04-24 16:26:30 -0300

    Login e Cadastro em PHP criados, APACHE mudado para conseguir ler todas as pastas.

---
+++ PHP/cadastro.php
@@ -0,0 +1,37 @@
+<?php
+
+require_once 'conexao.php';
+
+if ($_SERVER["REQUEST_METHOD"] === "POST"){
+    $nome  = $_POST['nome'] ?? '';
+    $email = $_POST['email'] ?? '';
+    $senha = $_POST['senha'] ?? '';
+    $confirmarSenha = $_POST ['confirmar_senha'] ?? '';
+
+    if ($senha !== $confirmarSenha){
+        echo "As senhas não coincidem.";
+        exit;
+    }
+
+    $SenhaHash = password_hash($senha, PASSWORD_DEFAULT);
+
+
+    try {
+        $stmt = $pdo->prepare("INSERT INTO usuarios (nome, email, senha) VALUES (:nome, :email, :senha)");
+        $stmt->bindParam(':nome', $nome);
+        $stmt->bindParam(':email', $email);
+        $stmt->bindParam(':senha', $SenhaHash);
+
+        $stmt->execute();
+        
+        echo "Cadastro realizado com sucesso!";
+    }
+    catch (PDOException $e){
+        echo "Erro ao cadastrar o usuario: " . $e->getMessage();
+    }
+    
+}   else {
+    echo "Método inválido.";
+}
+?>
+
---
+++ PHP/login.php
@@ -0,0 +1,30 @@
+<?php
+session_start();
+
+require_once 'conexao.php';
+
+if($_SERVER['REQUEST_METHOD'] === 'POST'){
+    $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
+    $senha = $_POST['senha'];
+
+    if (!empty($email) && !empty($senha)){
+        $stmt = $pdo->prepare("SELECT * FROM usuarios WHERE email = :email");
+        $stmt->bindParam(':email', $email);
+        $stmt->execute();
+
+        $usuario = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        if ($usuario && password_verify($senha, $usuario['senha'])){
+            $_SESSION['usuario_id'] = $usuario['id'];
+            $_SESSION['usuario_nome'] = $usuario['nome'];
+
+            echo "Login feito com sucesso";
+            header('Location: painel.php');
+            exit;
+        } else{
+            echo "Email ou senha inválidos!";
+            }
+        } else {
+            echo "Preencha todos os campos!";
+            }
+}
