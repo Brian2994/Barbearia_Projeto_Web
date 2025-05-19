03833ba72868b0e89b09843e3d3605b3b7b8add7
Samuel Santos <samueldsantos@ipt.br>
2025-05-19 16:55:10 -0300

    Adicionei umas mudanças para o site ficar responsivo

--- HTML/assets/js/script_formulario.js
+++ HTML/assets/js/script_formulario.js
@@ -7,4 +7,21 @@ function mostrarLogin() {
     document.getElementById("loginForm").classList.remove("active");
     document.getElementById("cadastroForm").classList.add("active");
   }
-  
+  function toggleMenu() {
+    const menu = document.getElementById('menu');
+    menu.classList.toggle('show');
+}
+    document.querySelectorAll('.navbar a').forEach(link => {
+    link.addEventListener('click', () => {
+    document.getElementById('menu').classList.remove('show');
+    });
+});
+    document.addEventListener('click', function(e) {
+    const menu = document.getElementById('menu');
+    const hamburger = document.querySelector('.hamburguer');
+    
+    if (!menu.contains(e.target) && !hamburger.contains(e.target)) {
+    menu.classList.remove('show');
+}
+});
+
